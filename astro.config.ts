dbde45c4d110dea561b01b097652052142c4fa06
root <Nex Zhu>
2023-10-14 17:43:39 +0800

    setup: astro.config.js -> astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
 import vue from '@astrojs/vue'
 
-// eslint-disable-next-line import/no-unresolved
 import viteConfig from './viteConfig'
 
 // https://astro.build/config
