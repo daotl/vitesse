4e7845aa213c5f8efe961e8295c2a83161e43bab
Nex <hitnexup@gmail.com>
2023-10-17 13:32:21 +0800

    refactor: add back Vue entrypoint to setup modules

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,16 @@
 import vue from '@astrojs/vue'
 import { defineConfig } from 'astro/config'
+import UnoCSS from 'unocss/astro'
 
-import viteConfig from './viteConfig'
+import vite from './viteConfig'
 
 // https://astro.build/config
 export default defineConfig({
-  // Enable Vue to support Vue components.
   integrations: [
     vue({
-      appEntrypoint: '/src/main',
+      appEntrypoint: '/src/vue',
     }),
+    UnoCSS({ injectReset: true }),
   ],
-  vite: viteConfig,
+  vite,
 })
---
+++ src/vue.ts
@@ -0,0 +1,14 @@
+import 'uno.css'
+import '@unocss/reset/tailwind.css'
+import './styles/main.css'
+
+import type { App } from 'vue'
+
+import type { UserModule } from './types'
+
+export default function setup(app: App<Element>) {
+  // install all modules under `modules/`
+  Object.values(import.meta.glob<{ install: UserModule }>('./modules/*.ts', { eager: true }))
+    .forEach((i) => i.install?.(app))
+  // ctx.app.use(Previewer)
+}
